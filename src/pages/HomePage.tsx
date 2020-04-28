99ff5d110dc5926a381723887715d75b6b6cad4c
Darren King <dking949@gmail.com>
2020-04-28 13:09:01 +1000

    embed twitter timeline

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react"
 
 export default class HomePage extends Component {
+
+    componentDidMount() {
+        const script = document.createElement("script");
+
+        script.src = "https://platform.twitter.com/widgets.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+    }
     
-    public render() {
+    public render() { 
         return(
             
             <div>
-               <a className="twitter-timeline" data-width="300" data-theme="dark"
-               href="https://twitter.com/rosemountgaa?ref_src=twsrc%5Etfw">Tweets by rosemountgaa</a> 
-               <script async src="https://platform.twitter.com/widgets.js" data-charset="utf-8"></script>
+               <a className="twitter-timeline" data-width="300" data-height="400" data-theme="dark"
+               href="https://twitter.com/rosemountgaa?ref_src=twsrc%5Etfw">Tweets by rosemountgaa</a>
             </div>
         );
     }
