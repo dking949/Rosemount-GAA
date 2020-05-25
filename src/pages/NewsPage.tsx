5800f7892941f8d2ebd7f8a1787b4ad52fad4360
Darren King <dking949@gmail.com>
2020-05-25 18:46:11 +1000

    Fixed typo in package json

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -3,6 +3,15 @@ import React, { Component } from "react"
 export default class NewsPage extends Component {
     
     public render() {
-        return(<h1>This is the NewsPage</h1>);
+        return(
+          <div>
+            <h1>This is the NewsPage</h1>
+            <p>This page is currently under construction.
+              <br/>
+              The finished product will show a newsfeed/list of form entries displaying local news.
+              The content will be fetched from DynamoDB table that only an admin will have write access to.
+            </p>
+          </div>
+        );
     }
 }
